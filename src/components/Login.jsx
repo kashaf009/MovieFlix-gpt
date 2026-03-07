9332e2a6d040102cb3749450c5f9e1b21d233250
kashaf khan <mdkashaf9931@gmail.com>
2026-03-07 23:36:08 +0530

    improved login page ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,31 +23,35 @@
 
 // export default Login;
 
+// 2
+
 import React from "react";
 import Nav from "./Nav";
 import Signin from "./Signin";
 
 const Login = () => {
   return (
-    <div className="relative h-screen w-full">
-      <Nav />
+    <div className="relative h-[120vh] w-full">
 
       {/* Background Image */}
       <img
+        src="/img/home-img.png"
+        alt="background"
         className="absolute top-0 left-0 w-full h-full object-cover"
-        src="img/home-img.png"
-        alt=""
       />
 
-      {/* Gradient Overlay */}
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black via-black/40 to-transparent"></div>
+      {/* Netflix Style Dark Gradient */}
+      <div className="absolute inset-0 bg-gradient-to-t from-black via-black/55 to-black/35"></div>
 
-      {/* Content */}
+      {/* Content Layer */}
       <div className="relative z-10">
+        <Nav />
         <Signin />
       </div>
+
     </div>
   );
 };
 
 export default Login;
+
